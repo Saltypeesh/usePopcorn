1f4102059ddd130722a2f45ef4f4f53ed7a5ea95
Saltypeesh <tungthanh254@gmail.com>
2024-02-01 04:20:23 +0700

    first section

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import './index.css';
+// import App from './App';
+
+import StarRating from "./StarRating";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <StarRating
+      maxRating='{5sdfsd}'
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+
+    
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
+  </React.StrictMode>
+);
